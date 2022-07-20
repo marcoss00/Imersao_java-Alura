a0a887b743fe5c823471bc8a848075cc28a63aa3
marcoss00 <marcossvinc@gmail.com>
2022-07-20 09:15:51 -0300

    aula1 - consumindo api de filmes

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        var response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // pegar só os dados que interessam (título, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaFilmes = parser.parse(body);
+
+        // exibir e manipular os dados
+        for (Map<String, String> filme : listaFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+}
